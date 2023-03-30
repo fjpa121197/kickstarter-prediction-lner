131f009aeeccc3cdb7acb489056a494de6806764
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2023-03-30 19:09:54 +0100

    Finished simple working version of script that handle the orchestrate the whole ML pipeline. It handles preprocessing data (data spliting), model training and storing.

--- ml_pipeline/run_pipeline.py
+++ ml_pipeline/run_pipeline.py
@@ -1,28 +1,16 @@
 import argparse
 import sys
-import datetime
 import traceback
 import os
-import logging
 
-# Gets or creates a logger
-logger = logging.getLogger(__name__)  
 
-# set log level
-logger.setLevel(logging.DEBUG)
-
-# define file handler and set formatter
-file_handler = logging.FileHandler('logfile.log')
-formatter    = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
-file_handler.setFormatter(formatter)
-
-# add file handler to logger
-logger.addHandler(file_handler)
+from pipeline.preprocess import Preprocessor
+from pipeline.train import NNRegressor
 
 
 def main():
     
-    logger.info("Initiating ML pipeline run ...")
+    print("Initiating ML pipeline run ...")
     
     parser = argparse.ArgumentParser(
         description="ML Pipeline, triggers a ML pipeline that trains a neural network based on provided a data file path."
@@ -33,31 +21,36 @@ def main():
         "--data-file",
         dest="data_file_path",
         type=str,
-        required=False,
+        required=True,
         help="""Data to be used in the pipeline, must be a path.""",
     )
     
     # Get arguments passed when invoking option
     # As for know, it only takes a data file path argument (which must be locally)
     args = parser.parse_args()
-
-
-    # TODO: Check file path exists and there is a data file to work with before going into try/except block. This can be a function in utils module.
-
+    
+    # Check file exists in path
+    if os.path.isfile(args.data_file_path):
+        data_file_path = args.data_file_path
+        
+    else:
+        print("File path is not valid. Please check directory and file exists.")
+        sys.exit(1)
     
     try:
         
-        # TODO: Call pipeline.preprocess so it preprocesses data (limited to separation of features/target and train_test_split)
+        # Preprocessing Step
+        preprocessor = Preprocessor(data_file_path=data_file_path, target = 'usd_smooth_pledged_per_day', 
+                                    random_state= 212, index_col='ID')
+        X_train, X_test, y_train, y_test = preprocessor.split_dataset(test_size = 64000) # split dataset into train and test set
         
-        # TODO: Call pipeline.train so it initializes model with the necessary configurations and ready to be trained
+        # Training Step
+        trainer = NNRegressor() # Initialize NN model using default architecture
+        trainer.fit(X_train, y_train, epochs=2) # Train model using training set and for specific number of epochs
+        preds = trainer.predict(X_test) # Get predictions using trained model
+        # Save artifacts Step
+        trainer.save_model()
         
-        # TODO: Call pipeline.train so it trains model
-        
-        # TODO: Call pipeline.eval so it uses trained model to predict and evaluate predictions based on test set generated in preprocessing stage
-        
-        # TODO: Call pipeline.train so it saves trained model to disk
-        
-        print("Hi")
     except Exception as e:
         print(f"Exeception: {e}")
         traceback.print_exc()
