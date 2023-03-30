0ca2916d06ad790622d8937630e7257ffd61503b
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2023-03-30 19:11:47 +0100

    bash script that automates the creation of the docker image, run container, and transfer of training artifacts from container to local machine

---
+++ run_pipeline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to pipeline directory
+cd ml_pipeline
+
+# Start docker steps, starting with building image and then running container
+docker build --tag lner-ml-pipeline .
+docker run lner-ml-pipeline
+
+# Access docker container files and copy artifacts (in this case, the trained model)
+CONTAINER_ID=$(docker ps -alq)
+mkdir -p artifacts
+docker cp $CONTAINER_ID:ml-pipeline/model_1.h5 artifacts/
