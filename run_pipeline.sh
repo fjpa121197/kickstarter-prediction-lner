efdb8dc9eb3fe3237bbee49f372dc9643164d539
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2023-03-31 12:45:39 +0100

    fixed end of line issue because editing it in windows

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 # Navigate to pipeline directory
 cd ml_pipeline
-
 # Start docker steps, starting with building image and then running container
 docker build --tag lner-ml-pipeline .
 docker run lner-ml-pipeline
-
 # Access docker container files and copy artifacts (in this case, the trained model)
 CONTAINER_ID=$(docker ps -alq)
 mkdir -p artifacts
