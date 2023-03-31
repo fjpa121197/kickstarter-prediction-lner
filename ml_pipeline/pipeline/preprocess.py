f8773b1d006a2f06839e5440cdd2fa3fb4f9c72f
fjpa121197 <francisco.parrilla.andrade@gmail.com>
2023-03-31 12:23:25 +0100

    adding comments to class and methods

--- ml_pipeline/pipeline/preprocess.py
+++ ml_pipeline/pipeline/preprocess.py
@@ -2,6 +2,22 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 class Preprocessor():
+    """
+    Create a Preprocessor object that handles data preparation steps needed to transform and prepare data for next steps in a ML cycle.
+    It takes a dataframe as input, and it handles data loading automatically.
+    
+    Parameters
+    ----------
+    
+    data_file_path : path of file to be as data (str).
+    
+    target : name of the column in data to be used as target (str).
+    
+    random_state : number to be used as seed to get reproducible results (int).
+    
+    index_col : name of the column to be used in data as index (str - optional).
+    
+    """
     def __init__(self, data_file_path : str, target : str, random_state: int, index_col : str = None):
         
         print("Initializing data preprocessor ... ")
@@ -22,6 +38,15 @@ class Preprocessor():
         
         
     def _load_file_dataframe(self, path : str):
+        """
+        Load csv data file into a dataframe (given that path is correct). For internal use only.
+
+        Args:
+            path (str): csv data file path
+
+        Returns:
+            df: DataFrame
+        """
         
         df = pd.read_csv(path, index_col=self.index_col)
         
@@ -29,6 +54,16 @@ class Preprocessor():
     
     
     def _separate_features_target(self, df):
+        """
+        Given a dataframe, separates features and target. For internal use only.
+
+        Args:
+            df (pd.DataFrame): dataframe to be separated
+
+        Returns:
+            X: dataframe only containing features
+            y: dataframe only containing target
+        """
         
         X = df.drop(self.target, axis = 1)
         y = df[self.target]
@@ -36,6 +71,19 @@ class Preprocessor():
         return X, y
     
     def split_dataset(self, test_size):
+        """
+        Separate dataframe into train and test set. Given object was initialized correctly, it will separate data into 2 sets, 
+        train and test set, based on test_size parameter.
+
+        Args:
+            test_size (int): Number of samples needed in test set (can be integer or a ratio (0.1, 0.2, 0.3, ...)).
+
+        Returns:
+            X_train: training data containing features only.
+            X_test: testing data containing features only.
+            y_train: training data containing target only.
+            y_test: test data containing target only.
+        """
         
         df = self.data_df
         df = df.dropna(subset=[self.target])
